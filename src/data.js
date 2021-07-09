30d1a8f380cc2bada70f6fd4deaaa8ee4f4fb75b
manaswini2001tech <manasiwini@gmail.com>
2021-07-09 19:17:55 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Cartoon 1',
+    age: 19,
+    image:
+      'https://cdn.imgbin.com/17/22/22/imgbin-graphic-designer-illustrator-drawing-portrait-solitaire-bird-in-rodrigues-1bAxEVWMwCPVf96gzDmwzGYUW.jpg',
+  },
+  {
+    id: 2,
+    name: 'Cartoon 2',
+    age: 12,
+    image:
+      'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/142819271/original/09dafa4104fa6aeca4e62f33326be4933ae7ccac/create-cartoon-profile-picture-abd7.jpg',
+  },
+  {
+    id: 3,
+    name: 'Cartoon 3',
+    age: 26,
+    image:
+      'https://www.whatsappimages.in/wp-content/uploads/2021/01/Cartoon-Profile-Images-for-Whatsapp-DP-14.jpg',
+  },
+  {
+    id: 4,
+    name: 'Cartoon 4',
+    age: 24,
+    image:
+      'https://i.pinimg.com/originals/a2/e1/8c/a2e18cbfbcaa8756fe5b40f472eeff45.jpg',
+  },
+  {
+    id: 5,
+    name: 'Cartoon 5',
+    age: 29,
+    image:
+      'https://i.pinimg.com/originals/28/07/8e/28078e60a32735ccec00e3215c0d035f.jpg',
+  },
+];
